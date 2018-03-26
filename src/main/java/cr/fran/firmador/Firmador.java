dec7856a70e4e9bbf820e2b07036835a3e8e3c84
Francisco de la Peña <fran@fran.cr>
2018-03-25 23:26:11 -0600

    Importar ejemplo de PKCS11, todavía requiere callback

--- src/main/java/cr/fran/firmador/Firmador.java
+++ src/main/java/cr/fran/firmador/Firmador.java
@@ -1,13 +1,29 @@
 package cr.fran.firmador;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
+import eu.europa.esig.dss.DigestAlgorithm;
+import eu.europa.esig.dss.SignatureValue;
+import eu.europa.esig.dss.ToBeSigned;
+import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
+import eu.europa.esig.dss.token.Pkcs11SignatureToken;
+import eu.europa.esig.dss.token.SignatureTokenConnection;
+import eu.europa.esig.dss.utils.Utils;
+
 public class Firmador
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SignatureTokenConnection token = new Pkcs11SignatureToken("/usr/lib/x64-athena/libASEP11.so");
+
+        List<DSSPrivateKeyEntry> keys = token.getKeys();
+        for (DSSPrivateKeyEntry entry : keys) {
+            System.out.println(entry.getCertificate().getCertificate());
+        }
+
+        ToBeSigned toBeSigned = new ToBeSigned("Hello world".getBytes());
+        SignatureValue signatureValue = token.sign(toBeSigned, DigestAlgorithm.SHA256, keys.get(0));
+
+        System.out.println("Signature value : " + Utils.toBase64(signatureValue.getValue()));
     }
 }
