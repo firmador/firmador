1273cc27b581b8e332aad49aaedef050ec118981
Francisco de la Peña <fran@fran.cr>
2018-03-25 21:49:36 -0600

    Generar POM inicial mediante archetype:generate de Maven

---
+++ src/main/java/cr/fran/firmador/App.java
@@ -0,0 +1,13 @@
+package cr.fran.firmador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
