87d174c7059ca3c2bfa81e7813ff5f559d69e810
Francisco de la Peña <fran@fran.cr>
2018-03-26 00:13:48 -0600

    Solicitar PIN y usar el método que usa PrefilledPasswordCallback

--- src/main/java/cr/fran/firmador/Firmador.java
+++ src/main/java/cr/fran/firmador/Firmador.java
@@ -1,5 +1,7 @@
 package cr.fran.firmador;
 
+import java.io.Console;
+import java.util.Arrays;
 import java.util.List;
 
 import eu.europa.esig.dss.DigestAlgorithm;
@@ -14,7 +16,11 @@ public class Firmador
 {
     public static void main( String[] args )
     {
-        SignatureTokenConnection token = new Pkcs11SignatureToken("/usr/lib/x64-athena/libASEP11.so");
+        Console console = System.console();
+        console.printf("PIN: ");
+        char[] pin = console.readPassword();
+
+        SignatureTokenConnection token = new Pkcs11SignatureToken("/usr/lib/x64-athena/libASEP11.so", pin);
 
         List<DSSPrivateKeyEntry> keys = token.getKeys();
         for (DSSPrivateKeyEntry entry : keys) {
@@ -23,6 +29,7 @@ public class Firmador
 
         ToBeSigned toBeSigned = new ToBeSigned("Hello world".getBytes());
         SignatureValue signatureValue = token.sign(toBeSigned, DigestAlgorithm.SHA256, keys.get(0));
+        Arrays.fill(pin, ' ');
 
         System.out.println("Signature value : " + Utils.toBase64(signatureValue.getValue()));
     }
