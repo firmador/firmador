0b011954985a7b2cea30d90c4847986ad7029875
Francisco de la Peña <fran@fran.cr>
2021-12-20 00:50:24 -0600

    PDFBox cache warmup

--- src/main/java/Firmador.java
+++ src/main/java/Firmador.java
@@ -19,12 +19,16 @@ along with Firmador.  If not, see <http://www.gnu.org/licenses/>.  */
 
 import javax.swing.SwingUtilities;
 
+import org.apache.pdfbox.pdmodel.font.FontMappers;
+
 import cr.libre.firmador.gui.GUIInterface;
 import cr.libre.firmador.gui.GUISelector;
 
 public class Firmador {
 
     public static void main(String[] args) {
+        // PDFBox font cache warmup
+        FontMappers.instance().getFontBoxFont(null, null);
         // Workaround illegal access for Java 9+ until jaxb 2.4.0 gets released
         System.setProperty("com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "true");
         GUISelector guiselector = new GUISelector();
