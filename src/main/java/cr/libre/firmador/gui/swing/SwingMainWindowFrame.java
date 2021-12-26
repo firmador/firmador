bd4f4fbd7b26ecf450bd2adf6e80197f7e6ee7b6
Luis Zárate M <luis.zarate@solvosoft.com>
2021-12-25 22:24:12 -0600

    License in the file header

--- src/main/java/cr/libre/firmador/gui/swing/SwingMainWindowFrame.java
+++ src/main/java/cr/libre/firmador/gui/swing/SwingMainWindowFrame.java
@@ -1,3 +1,22 @@
+/* Firmador is a program to sign documents using AdES standards.
+
+Copyright (C) 2021 Firmador authors.
+
+This file is part of Firmador.
+
+Firmador is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Firmador is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Firmador.  If not, see <http://www.gnu.org/licenses/>.  */
+
 package cr.libre.firmador.gui.swing;
 
 import java.awt.HeadlessException;
