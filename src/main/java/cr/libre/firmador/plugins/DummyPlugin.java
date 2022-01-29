48f469aab91147e037a44c2640a2d8868c3c5a70
Luis Zárate M <luis.zarate@solvosoft.com>
2022-01-29 13:25:40 -0600

    add CADES extends

--- src/main/java/cr/libre/firmador/plugins/DummyPlugin.java
+++ src/main/java/cr/libre/firmador/plugins/DummyPlugin.java
@@ -18,19 +18,11 @@ You should have received a copy of the GNU General Public License
 along with Firmador.  If not, see <http://www.gnu.org/licenses/>.  */
 package cr.libre.firmador.plugins;
 
-import java.security.KeyStore;
-
-import cr.libre.firmador.CRSigner;
-import eu.europa.esig.dss.token.Pkcs11SignatureToken;
 
 public class DummyPlugin implements Plugin {
 
 	public void start() {
 		System.out.println("Stating DummyPlugin");
-		//KeyStore keyStore = KeyStore.getInstance(SUN_PKCS11_KEYSTORE_TYPE, getProvider());
-		//signingToken = new Pkcs11SignatureToken(getPkcs11Lib(), "");
-		
-		CRSigner crsigner  = new CRSigner(null);
 	}
 
 	public void stop() {
