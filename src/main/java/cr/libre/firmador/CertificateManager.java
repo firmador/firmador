8fefa25d62965087e5fc78789b0c193e382f2c21
Luis Zárate Montero <luis.zarate@solvosoft.com>
2023-11-10 17:36:02 -0600

    Supported methods that will be used for CRSigner

--- src/main/java/cr/libre/firmador/CertificateManager.java
+++ src/main/java/cr/libre/firmador/CertificateManager.java
@@ -1,45 +1,57 @@
 package cr.libre.firmador;
 
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 import java.util.List;
 
 import eu.europa.esig.dss.model.x509.CertificateToken;
 import eu.europa.esig.dss.spi.DSSUtils;
 import eu.europa.esig.dss.spi.x509.CertificateSource;
 import eu.europa.esig.dss.spi.x509.CommonCertificateSource;
+import eu.europa.esig.dss.spi.x509.CommonTrustedCertificateSource;
 
 public class CertificateManager {
     private CertificateSource certSource = new CommonCertificateSource();
 
     public CertificateManager() {
-        certSource.addCertificate(DSSUtils.loadCertificate(this.getClass().getClassLoader()
-                .getResourceAsStream("certs/CA POLITICA PERSONA FISICA - COSTA RICA v2.crt")));
-        certSource.addCertificate(DSSUtils.loadCertificate(this.getClass().getClassLoader()
-                .getResourceAsStream("certs/CA POLITICA PERSONA JURIDICA - COSTA RICA v2.crt")));
-        certSource.addCertificate(DSSUtils.loadCertificate(this.getClass().getClassLoader()
-                .getResourceAsStream("certs/CA POLITICA SELLADO DE TIEMPO - COSTA RICA v2.crt")));
+        ClassLoader loaderclass = this.getClass().getClassLoader();
         certSource.addCertificate(DSSUtils.loadCertificate(
-                this.getClass().getClassLoader().getResourceAsStream("certs/CA SINPE - PERSONA FISICA v2.cer")));
+                loaderclass.getResourceAsStream("certs/CA POLITICA PERSONA FISICA - COSTA RICA v2.crt")));
         certSource.addCertificate(DSSUtils.loadCertificate(
-                this.getClass().getClassLoader().getResourceAsStream("certs/CA SINPE - PERSONA FISICA v2(1).crt")));
+                loaderclass.getResourceAsStream("certs/CA POLITICA PERSONA JURIDICA - COSTA RICA v2.crt")));
         certSource.addCertificate(DSSUtils.loadCertificate(
-                this.getClass().getClassLoader().getResourceAsStream("certs/CA SINPE - PERSONA FISICA v2(2).crt")));
+                loaderclass.getResourceAsStream("certs/CA POLITICA SELLADO DE TIEMPO - COSTA RICA v2.crt")));
         certSource.addCertificate(DSSUtils.loadCertificate(
-                this.getClass().getClassLoader().getResourceAsStream("certs/CA SINPE - PERSONA JURIDICA v2.cer")));
+                loaderclass.getResourceAsStream("certs/CA SINPE - PERSONA FISICA v2.cer")));
         certSource.addCertificate(DSSUtils.loadCertificate(
-                this.getClass().getClassLoader().getResourceAsStream("certs/CA SINPE - PERSONA JURIDICA v2(1).crt")));
+                loaderclass.getResourceAsStream("certs/CA SINPE - PERSONA FISICA v2(1).crt")));
         certSource.addCertificate(DSSUtils.loadCertificate(
-                this.getClass().getClassLoader().getResourceAsStream("certs/CA SINPE - PERSONA JURIDICA v2(2).crt")));
+                loaderclass.getResourceAsStream("certs/CA SINPE - PERSONA FISICA v2(2).crt")));
+        certSource.addCertificate(DSSUtils.loadCertificate(
+                loaderclass.getResourceAsStream("certs/CA SINPE - PERSONA JURIDICA v2.cer")));
+        certSource.addCertificate(DSSUtils.loadCertificate(
+                loaderclass.getResourceAsStream("certs/CA SINPE - PERSONA JURIDICA v2(1).crt")));
+        certSource.addCertificate(DSSUtils.loadCertificate(
+                loaderclass.getResourceAsStream("certs/CA SINPE - PERSONA JURIDICA v2(2).crt")));
         certSource.addCertificate(DSSUtils
-                .loadCertificate(this.getClass().getClassLoader().getResourceAsStream("certs/TSA SINPE v2.cer")));
+                .loadCertificate(loaderclass.getResourceAsStream("certs/TSA SINPE v2.cer")));
         certSource.addCertificate(DSSUtils.loadCertificate(
-                this.getClass().getClassLoader().getResourceAsStream("certs/CA RAIZ NACIONAL - COSTA RICA v2.crt")));
+                loaderclass.getResourceAsStream("certs/CA RAIZ NACIONAL - COSTA RICA v2.crt")));
         certSource.addCertificate(DSSUtils.loadCertificate(
-                this.getClass().getClassLoader().getResourceAsStream("certs/CA RAIZ NACIONAL COSTA RICA.cer")));
+                loaderclass.getResourceAsStream("certs/CA RAIZ NACIONAL COSTA RICA.cer")));
 
     }
 
-    public CertificateSource getCertificateChainCertificateSource() {
-        return null;
+    public CertificateSource getCertificateChainCertificateSource(CertificateToken subjectCertificate) {
+        List<CertificateToken> certs = this.getCertificateChain(subjectCertificate);
+        CertificateSource certificatesource = null;
+        if (certs != null) {
+            certificatesource = new CommonCertificateSource();
+            for (CertificateToken cert : certs) {
+                certificatesource.addCertificate(cert);
+            }
+        }
+        return certificatesource;
     }
 
     public List<CertificateToken> getCertificateChain(CertificateToken subjectCertificate) {
@@ -51,4 +63,39 @@ public class CertificateManager {
         return null;
     }
 
+    public List<X509Certificate> getX509CertificateChain(CertificateToken subjectCertificate) {
+        List<X509Certificate> certlist = null;
+        List<CertificateToken> certs = this.getCertificateChain(subjectCertificate);
+        if (certs != null) {
+            certlist = new ArrayList<X509Certificate>();
+            for (CertificateToken cert : certs) {
+                certlist.add(cert.getCertificate());
+            }
+        }
+        return certlist;
+    }
+
+    public CommonTrustedCertificateSource getTrustedCertificateSource(CertificateToken subjectCertificate) {
+        CommonTrustedCertificateSource commonTrustedCertificateSource = null;
+        List<CertificateToken> certs = this.getCertificateChain(subjectCertificate);
+        if (certs != null && certs.size() > 0) {
+            commonTrustedCertificateSource = new CommonTrustedCertificateSource();
+            commonTrustedCertificateSource.addCertificate(certs.get(0));
+        }
+        return commonTrustedCertificateSource;
+    }
+
+    public CertificateSource getAdjunctCertSource(CertificateToken subjectCertificate) {
+        CertificateSource adjunctCertSource = null;
+        List<CertificateToken> certs = this.getCertificateChain(subjectCertificate);
+        if (certs != null && certs.size() > 0) {
+            adjunctCertSource = new CommonCertificateSource();
+            certs.remove(0); // Remove Root CA certificate, this would by on TrustedCertificateSource
+            for (CertificateToken cert : certs) {
+                adjunctCertSource.addCertificate(cert);
+            }
+        }
+
+        return adjunctCertSource;
+    }
 }
