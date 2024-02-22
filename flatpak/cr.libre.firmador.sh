d9e4502a260a612d3fd391e485dc35b023240063
Luis Zárate Montero <luis.zarate@solvosoft.com>
2024-02-21 23:25:01 -0600

    Include Libreoffice Appimage and change SDK to gnome SDK, now Libreoffice work well and preview is ok on all formats

--- flatpak/cr.libre.firmador.sh
+++ flatpak/cr.libre.firmador.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # create env variables required for it to work fine
 export LIBASEP11=/app/lib/libASEP11.so
 export FIRMADORINFLATPAK=true
-export FLATPAKSOFFICEPATH=/var/run/host/usr/bin/soffice
+export FLATPAKSOFFICEPATH=/app/libreoffice/opt/libreoffice7.5/program/soffice
 
 # Move to the right folder / Run Firmador
 cd "$SCRIPT_DIR"
